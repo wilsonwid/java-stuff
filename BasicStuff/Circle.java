9652dcd699114958525f2f0fd0bfe08e856cfd7f
Wilson Widyadhana <wilsonwidyadhana681@gmail.com>
2021-12-13 20:30:12 +0800

    added circle.java

--- BasicStuff/Circle.java
+++ BasicStuff/Circle.java
@@ -1,18 +1,46 @@
 package BasicStuff;
 
+class Point {
+    private final double x;
+    private final double y;
+
+    Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public String toString() {
+        return "Point at (" + this.x + ", " + this.y + ")";
+    }
+}
+
 class Circle {
     
     // attribute
     private final double radius;
-    private final double centre;
+    private final Point centre;
 
-    Circle(double radius, double centre) {
+    Circle(double radius, Point centre) {
         this.radius = radius;
         this.centre = centre;
     }
 
-    public void enlarge(double factor) {
-        this.radius *= factor;
+    public Circle enlarge(double factor) {
+        return new Circle(this.radius*factor, this.centre);
+    }
+
+    
+
+    public String toString() {
+        return "Circle with centre (";
     }
 
     
